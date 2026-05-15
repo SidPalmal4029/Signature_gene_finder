849cdaf5010f826e548ff478791c5d98e06ea7d3
Siddhakam Palmal <sidpalmal@gmail.com>
2026-05-16 00:08:52 +0530

    Create the launcher
    The launcher for the program was created.

--- signature-gene-finder.sh
+++ signature-gene-finder.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
 
+set -e
+
+usage() {
+  echo "Signaturegenefinder"
+  echo ""
+  echo "Usage:"
+  echo "  run_signaturegenefinder.sh -i <genome_dir> -o <output_dir> -g <outgroup_name>"
+  echo ""
+  exit 1
+}
+
+while getopts "i:o:g:" opt; do
+  case $opt in
+    i) INPUT_DIR="$OPTARG" ;;
+    o) OUT_DIR="$OPTARG" ;;
+    g) OUTGROUP="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+[ -z "$INPUT_DIR" ] && usage
+[ -z "$OUT_DIR" ] && usage
+[ -z "$OUTGROUP" ] && usage
+
+mkdir -p "$OUT_DIR"
+
+python3 signaturegenefinder.py \
+  --input "$INPUT_DIR" \
+  --output "$OUT_DIR" \
+  --outgroup "$OUTGROUP"
