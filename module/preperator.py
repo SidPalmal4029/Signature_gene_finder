23a066af116c615f00eca9c7b6c3c25c02b75c03
Siddhakam Palmal <sidpalmal@gmail.com>
2026-05-17 20:15:30 +0530

    Change import statements to use 'Worker' directory

--- module/preperator.py
+++ module/preperator.py
@@ -1,12 +1,12 @@
 import argparse
 import os
 
-from worker.annotate_dfast import run_dfast_batch
-from worker.ogri_calc import run_ogri
-from worker.run_orthofinder import run_orthofinder
-from worker.reroot_tree import reroot_tree
-from worker.build_phylogroups import auto_phylogroups
-from worker.run_pangenome import run_pangenome
+from Worker.annotate_dfast import run_dfast_batch
+from Worker.ogri_calc import run_ogri
+from Worker.run_orthofinder import run_orthofinder
+from Worker.reroot_tree import reroot_tree
+from Worker.build_phylogroups import auto_phylogroups
+from Worker.run_pangenome import run_pangenome
 
 
 def main():
