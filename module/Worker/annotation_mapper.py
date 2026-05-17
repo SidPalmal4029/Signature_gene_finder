b4fc29b73ef06db0960b2615f3e51239ae503329
Siddhakam Palmal <sidpalmal@gmail.com>
2026-05-17 20:13:51 +0530

    Add function to merge annotations from a file

--- module/Worker/annotation_mapper.py
+++ module/Worker/annotation_mapper.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+
+def add_annotations(df, annotation_file):
+
+    annot = pd.read_csv(annotation_file, sep="\t")
+
+    return df.merge(
+        annot,
+        left_on="gene",
+        right_on="OG_ID",
+        how="left"
+    )
